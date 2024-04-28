671ef407b3aa56a12ae406422d14fcd66d559b00
Ryan <Ryan@localhost.com>
2024-04-28 09:19:13 +0700

    Move the home page to '/'

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,44 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <HelloWorld />
+  <v-container class="fill-height">
+    <v-responsive
+        class="align-centerfill-height mx-auto"
+        max-width="900"
+    >
+      <v-img
+          class="mb-4"
+          height="150"
+          src="@/assets/logo.png"
+      />
+      <div class="text-center">
+        <h4 class="text-h4 font-weight-bold">Welcome</h4>
+        <br/><br/><br/>
+      </div>
+      <div class="menu-button-group">
+        <v-btn
+            color="primary"
+            href="/login"
+        >
+          Login as admin
+        </v-btn>
+        <v-btn
+            color="primary"
+            href="/register"
+        >
+          Register
+        </v-btn>
+      </div>
+    </v-responsive>
+  </v-container>
 </template>
 
-<script lang="ts" setup>
-  //
-</script>
+<style scoped>
+.menu-button-group{
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+</style>
